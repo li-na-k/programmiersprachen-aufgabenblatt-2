17bf31f74b9d83c62f38e164b1b1ee20be1fb4c9
Lina Klass <linaklass@MacBook-Air.local>
2020-05-16 11:28:57 +0200

    task 2.2 added + tested

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,18 @@
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include "../external/catch-1.1/catch.hpp"
+#include "vec2.hpp"
+
+
+Vec2 a;
+Vec2 b{5.1f, -9.3f};
+
+//Aufgabe 2.2 - Default Memberinitialisierung testen
+TEST_CASE("Vec2 Initialisierung", "[Vec2]"){
+  REQUIRE(a.x == 0.0f);
+  REQUIRE(a.y == 0.0f); 
+  REQUIRE(5.1f == Approx(b.x));
+  REQUIRE(-9.3f == Approx(b.y));
+}
 
 int main(int argc, char *argv[])
 {
